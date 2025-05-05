e3c8f41ce438e9c2a0c3df5d4ec5c048d78e8b0c
Saymios <daniojenezis@gmail.com>
2025-05-05 12:24:24 +0300

    Header adaptive and mobile version ready

---
+++ src/common/customHooks/WindowResize.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export const useWindowResize = () => {
+    const [windowSize, setWindowSize] = useState({
+        width: typeof window !== 'undefined' ? window.innerWidth : 0,
+    });
+
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+              width: window.innerWidth,
+            });
+          };
+      
+          window.addEventListener('resize', handleResize);
+          return () => window.removeEventListener('resize', handleResize); // Cleanup
+     }, [])
+
+     return windowSize;
+}
+    
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom';
 import { Link } from "react-router-dom";
+
+import { useWindowResize } from "../../common/customHooks/WindowResize";
 
 import "./Header.css";
 
 function Header() {
+    const [isOpen, setIsOpen] = useState(false);
+    const { width } = useWindowResize();
+    const location = useLocation();
+
+
+    useEffect(() => {setIsOpen(false);}, [location.pathname]);
+
+
+    useEffect(() => {
+        if (width > 875 && isOpen) {
+            setIsOpen(false);
+        }
+    }, [width ,isOpen])
+
+    
     return (
         <header>
             <div className="base-links-container">
                 <div className='home-page-container'>
                     <Link to='/'>Головна</Link>
+                </div>
+
+                <div className="burger" id="burger" onClick={(e) => setIsOpen(!isOpen)}>
+                        <span></span>
+                        <span></span>
+                        <span></span>
                 </div>
 
                 <div className='base-links' id='base-links'>
@@ -21,6 +45,15 @@ function Header() {
                         <li><Link to='/registration'>Зарееструватися</Link></li>
                     </ul>
                 </div>
+
+                <ul className={`mobile-links ${isOpen ? 'active': ''}`} id="mobile-links" >
+                    <li><Link to='/menu'>Меню</Link></li>
+                    <li><Link to='/map'>Мапа кав'ярень</Link></li>
+                    <li><Link to='/search-reservations'>Мої бронювання</Link></li>
+                    <li><Link to='/profile'>Профіль</Link></li>
+                    <li><Link to='/login'>Увійти</Link></li>
+                    <li><Link to='/registration'>Зарееструватися</Link></li>
+                </ul>
             </div>
         </header>
     )
