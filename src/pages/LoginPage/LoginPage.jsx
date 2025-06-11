e4523739e7b819f25e97fb0583c687c4f3bbce3d
Saymios <daniojenezis@gmail.com>
2025-06-11 11:28:15 +0300

    add libs react-form-hook ,yup and update forms in login and registration page

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,60 +1,131 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from 'react-router-dom';
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import * as yup from 'yup';
 
 import { AuthContext } from "../../AuthContext";
 import GoggleIcon from "../../components/UI/icons/GoggleIcon/GoggleIcon";
 
 import "./LoginPage.css";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function LoginPage() {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
     const { login, error } = useContext(AuthContext);
     const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log('Form submitted:', { username, password})
-        const success = await login(username, password);
+    const schema = yup.object().shape({
+        username: yup
+            .string()
+            .min(2, 'Минимум 2 символа')
+            .required('Email обязателен')
+            .test('opti', 'Нужна хотя бы одна цифры в пароле', function (value) {
+                console.log(value)
+                const password = this.parent.password
+                return true
+            }),
+
+        password: yup
+            .string()
+            .min(2, 'В пароле не может быть меньше чем 2 символа')
+            .required('Email обязателен'),
+    })
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+        reset
+    } = useForm(
+        { resolver: yupResolver(schema) }
+    )
+
+    const onSubmit = async (data) => {
+        console.log("Данные", data);
+
+        const success = await login(data["username"], data["password"]);
 
         if (success) {
             console.log('Login successful!');
-            navigate('/'); // Redirect to the profile page or perform any other action
-            // Redirect to the profile page or perform any other action
+            navigate('/');
+
         } else {
             console.log('Login failed!');
         }
     }
 
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+    //     console.log('Form submitted:', { username, password})
+    //     const success = await login(username, password);
+
+    //     if (success) {
+    //         console.log('Login successful!');
+    //         navigate('/'); // Redirect to the profile page or perform any other action
+    //         // Redirect to the profile page or perform any other action
+    //     } else {
+    //         console.log('Login failed!');
+    //     }
+    // }
+
+    // {
+    //     Object.entries(errors).length !== 0 && (
+    //         <div>
+    //             <ul className="list-disc pl-5">
+    //                 {Object.entries(errors).map(([fieldName, error]) => (
+    //                     <li key={fieldName}>
+    //                         {fieldName} - {error.message} {/* Поле: {fieldName} - можно добавить при необходимости */}
+    //                     </li>
+    //                 ))}
+    //             </ul>
+    //         </div>
+    //     )
+    // }
+
     return (
         <div className='base-container'>
             <div className='form-login-container'>
-                <form onSubmit={handleSubmit} className="login-form"  aria-label="Login form">
+                <form onSubmit={handleSubmit(onSubmit)} className="login-form" aria-label="Login form">
                     <h3>Welcome Back!</h3>
+
                     <p className='second-title'>Don`n have account yet? <Link className='signuplink' to="/registration">Sign Up</Link></p>
 
                     <p className="form-label">
-                        <input 
+                        <input
+                            type="text"
+                            {...register('username')}
+                            id='username'
+                            placeholder='Логін aбо номер телефону'
+                            className='form-input'
+                            autoFocus
+                        />
+                        {/* <input 
                         type="text" 
                         id='username' 
                         name='username' 
                         placeholder='Логін aбо номер телефону' 
                         className='form-input' 
                         onChange={(e) => setUsername(e.target.value)}
-                        autoFocus/>
+                        autoFocus/> */}
                     </p>
 
                     <p className="form-label">
-                        <input 
+                        <input
+                            type="password"
+                            {...register('password')}
+                            id='password'
+                            placeholder='Пароль'
+                            className='form-input'
+                        />
+                        {/* <input 
                         type="password" 
                         id='password' 
                         name='password' 
                         placeholder='Пароль'
                         onChange={(e) => setPassword(e.target.value)} 
-                        className='form-input'/>
+                        className='form-input'/> */}
                     </p>
-                    
+
                     <button type='submit' className='submit-btn'>Увійти</button>
 
                     <div className="or">
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,35 +1,68 @@
 import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
 
 import { AuthContext } from "../../AuthContext";
 
 import DynamicImages from "../../components/UI/images/DynamicImages";
 import GoggleIcon from "../../components/UI/icons/GoggleIcon/GoggleIcon";
 import "./RegistrationPage.css";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function RegistrationPage() {
-    const [username, setUsername] = useState('');
-    const [phone, setPhone] = useState('');
-    const [formError, setFormError] = useState('');
-    const [password1, setPassword1] = useState('');
-    const [password2, setPassword2] = useState('');
     const { registration, error } = useContext(AuthContext);
     const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const shema = yup.object().shape({
+        'username': yup
+            .string()
+            .required('Поле обязательное')
+            .min(4, 'Логин не может быть меньше 4 символов'),
+
+        'phone': yup
+            .string()
+            .required('Поле обязательное')
+            .matches(/^(380|0)\d{9}$/, 'Неверный формат номера'),
         
-        if (password1 !== password2) {
-            alert("Паролі не співпадають!");
-            return;
-        }
+        'password1': yup
+            .string()
+            .required('Поле обязательное')
+            .test('pass', 'Пароль не может быть короче 6 символов', function(value) {
+                if (String(value).length < 6) {
+                    return false
+                }
+                
+                return true
+            }),
+        'password2': yup
+            .string()
+            .required('Поле обязательное')
+            .test('pass', 'Пароли должны совпадать', function(value) {
+                const password1 = this.parent.password1
 
-        if (password1.length < 8 || password2.length < 8) {
-            alert('Пароль повинен містити не менше 8 символів')
-        }
+                if (password1 === value) {
+                    return true
+                }
 
-        const success = registration(username, phone, password1, password2)
+                return false
+            })
+    })  
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+        reset
+    } = useForm({
+        resolver: yupResolver(shema)
+    })
+
+    const onSubmit =  async (data) => {
+        console.log(data)
+
+         const success = await registration(data['username'], data['phone'], data['password1'], data['password2'])
 
         if (success) {
             console.log('Registaration successful!');
@@ -39,52 +72,77 @@ function RegistrationPage() {
         }
     }
 
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault();
+        
+    //     if (password1 !== password2) {
+    //         alert("Паролі не співпадають!");
+    //         return;
+    //     }
+
+    //     if (password1.length < 8 || password2.length < 8) {
+    //         alert('Пароль повинен містити не менше 8 символів')
+    //     }
+
+    //     const success = registration(username, phone, password1, password2)
+
+    //     if (success) {
+    //         console.log('Registaration successful!');
+    //         navigate('/');
+    //     } else {
+    //         console.log('Registration failed!');
+    //     }
+    // }
+
 
     return (
         <div className="base-registration-container">
             <div className="registration-form-container">
-                <form id='registration-form' onSubmit={handleSubmit} aria-label="Registration form">
+                <form id='registration-form' onSubmit={handleSubmit(onSubmit)} aria-label="Registration form">
                     <h3>Sign Up</h3>
                     <p className="second-title">Do you have an account? <Link className="signuplink">Login</Link></p>
 
                     <input 
+                    {...register('username')}
                     type="text"
-                     name="username" 
                      className="registration-form-control" 
                      placeholder="Логін" 
-                     id="username" 
-                     maxLength="100" 
+                     id="username"  
                      autoFocus="" 
-                     onChange={(e) => setUsername(e.target.value)}
-                     required=""></input>
+                    //  onChange={(e) => setUsername(e.target.value)}
+                    //  required=""
+                     ></input>
 
                     <input 
+                    {...register('phone')}
                     type="text" 
-                    name="phone" 
                     className="registration-form-control" 
                     placeholder="Номер телефону" 
                     id="phone" 
                     maxLength="20" 
-                    onChange={(e) => setPhone(e.target.value)}
-                    required=""></input>
+                    // onChange={(e) => setPhone(e.target.value)}
+                    // required=""
+                    ></input>
 
                     <input 
+                    {...register('password1')}
                     type="password" 
-                    name="password1" 
                     className="registration-form-control" 
                     placeholder="Пароль" 
                     id="password1"
-                    onChange={(e) => setPassword1(e.target.value)} 
-                    required=""></input>
+                    // onChange={(e) => setPassword1(e.target.value)} 
+                    // required=""
+                    ></input>
 
                     <input 
+                    {...register('password2')}
                     type="password" 
-                    name="password2" 
                     className="registration-form-control" 
                     placeholder="Повторіть пароль" 
                     id="password2"
-                    onChange={(e) => setPassword2(e.target.value)}
-                    required=""></input>
+                    // onChange={(e) => setPassword2(e.target.value)}
+                    // required=""
+                    ></input>
 
                     <button type="submit" className="registrer-submit-btn">create account</button>
 
